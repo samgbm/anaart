7b62a5ea95f622da96813f62fd35391612271fec
samuel sanchez <samuelsan333@gmail.com>
2025-03-14 00:36:11 -0500

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,20 @@
+
+import ProductList from "@/components/shared/product/product-list";
+import { getLatestProducts } from "@/lib/actions/product.actions";
+
+const Homepage = async () => {
+
+  const latestProducts = await getLatestProducts();
+
+
+
+  return <>
+    <ProductList data={latestProducts} title='Newest Arrivals'/>
+  </>
+}
+
+export default Homepage;
+
+
+
+
