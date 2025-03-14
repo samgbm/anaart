1003959021d8f23ca5811ecde80c7e3ee85cafc4
samuel sanchez <samuelsan333@gmail.com>
2025-03-14 14:24:57 -0500

    callback-url-redirect

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -8,3 +8,10 @@ export const SERVER_URL =
 
 export const LATEST_PRODUCTS_LIMIT =
     Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
+
+
+
+export const signInDefaultValues = {
+    email: "",
+    password: "",
+};
