1e54a834e6acda262281d68dfbbd69bb07e0778a
samuel sanchez <samuelsan333@gmail.com>
2025-03-20 12:16:35 -0500

    fix page not found error

--- app/(root)/order/[id]/stripe-payment-success/page.tsx
+++ app/(root)/order/[id]/stripe-payment-success/page.tsx
@@ -1,18 +1,15 @@
-import { Metadata } from 'next';
+import { Button } from '@/components/ui/button';
+import { getOrderById } from '@/lib/actions/order.actions';
 import Link from 'next/link';
 import { notFound, redirect } from 'next/navigation';
 import Stripe from 'stripe';
-import { getOrderById } from '@/lib/actions/order.actions';
-import { Button } from '@/components/ui/button';
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
-export const metadata: Metadata = {
-    title: 'Stripe Payment Success',
-};
+
 const SuccessPage = async (props: {
     params: Promise<{ id: string }>;
     searchParams: Promise<{ payment_intent: string }>;
 }) => {
-    //  Get the order id and payment intent id from the URL
     const { id } = await props.params;
     const { payment_intent: paymentIntentId } = await props.searchParams;
 
@@ -20,10 +17,10 @@ const SuccessPage = async (props: {
     const order = await getOrderById(id);
     if (!order) notFound();
 
-    // Retrieve the payment intent
+    // Retrieve payment intent
     const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId);
 
-    // Check if the payment intent is valid
+    // Check if payment intent is valid
     if (
         paymentIntent.metadata.orderId == null ||
         paymentIntent.metadata.orderId !== order.id.toString()
@@ -31,18 +28,18 @@ const SuccessPage = async (props: {
         return notFound();
     }
 
-    // Check if the payment intent is successful
+    // Check if payment is successful
     const isSuccess = paymentIntent.status === 'succeeded';
 
     if (!isSuccess) return redirect(`/order/${id}`);
 
     return (
         <div className='max-w-4xl w-full mx-auto space-y-8'>
-            <div className='flex flex-col gap-6 items-center '>
+            <div className='flex flex-col gap-6 items-center'>
                 <h1 className='h1-bold'>Thanks for your purchase</h1>
-                <div>We are now processing your order.</div>
+                <div>We are processing your order.</div>
                 <Button asChild>
-                    <Link href={`/order/${id}`}>View order</Link>
+                    <Link href={`/order/${id}`}>View Order</Link>
                 </Button>
             </div>
         </div>
